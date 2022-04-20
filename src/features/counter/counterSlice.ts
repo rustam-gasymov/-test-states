87f691a78a3345408fa8cddce76c6f6f19bc6008
rustam-cloud <gasymovrustam86@Yandex.com>
2022-04-20 12:55:01 +0300

    feat: 2121212121

---
+++ src/features/counter/counterSlice.ts
@@ -0,0 +1,46 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface IState {
+  isLoading?: boolean;
+  isModalVisible?: boolean;
+  value?: number;
+  counter?: number;
+}
+
+const initialState = {
+  isModalVisible: false,
+  value: 0,
+  counter: 0,
+};
+
+export const reducers = {
+  setIncrementValue: (state: typeof initialState) => {
+    state.value++;
+  },
+  setNumber: (state: typeof initialState, { payload }: PayloadAction<number>) => {
+    console.log("settnumberits new reducer from slice", payload);
+    state.counter = state.counter + payload;
+  },
+  setDecrementValue: (state: typeof initialState, { payload }: PayloadAction) => {
+    console.log("decrement from decrement", payload);
+
+    state.value = state.value - 10;
+  },
+  changeVisibilityOfConfigureForm: (
+    state: typeof initialState,
+    { payload }: PayloadAction<boolean>
+  ) => {
+    state.isModalVisible = payload;
+  },
+};
+
+export const configureToolsSlice = createSlice({
+  name: "configureTools",
+  initialState,
+  reducers,
+});
+
+export const { setDecrementValue, setNumber, changeVisibilityOfConfigureForm, setIncrementValue } =
+  configureToolsSlice.actions;
+
+export default configureToolsSlice.reducer;
